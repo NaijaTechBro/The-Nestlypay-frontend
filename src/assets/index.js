11852bcfac40af566383c07687ca4e894ff8794c
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-03-13 14:04:46 +0100

    index

--- src/assets/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Payouts } from '../assets/payouts.png';
-export { default as MyInvoices } from '../assets/my-invoices.png';
-export { default as MyActivities } from '../assets/my-activities.png';
-export { default as InvoiceHistory } from '../assets/Invoice-history.png';
-export { default as Home } from '../assets/home.png';
-export { default as Clients } from '../assets/clients.png';
