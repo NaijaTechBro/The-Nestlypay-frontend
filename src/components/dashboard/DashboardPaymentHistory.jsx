23cb6b57867d9d849ac2c6fc16a0f24feef25697
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-03-13 14:07:47 +0100

    dashboard

---
+++ src/components/dashboard/DashboardPaymentHistory.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import Modalx from './Modalx'
+import '../styles/style'
+
+let status
+let method
+
+function DashboardPaymentHistory(props) {
+  const [open, setOpen] = useState(false)
+
+  const open_modal = () => {
+    setOpen((current) => !current)
+  }
+
+  if (props.status === 'Success') {
+    status = <span className="personaldashboard--success">Success</span>
+  } else if (props.status == 'Cancelled') {
+    status = <span className="personaldashboard--cancelled">Cancelled</span>
+  } else if (props.status == 'Pending') {
+    status = <span className="personaldashboard--pending">Pending</span>
+  }
+
+  if (props.method === 1) {
+    method = 'Bank Transfer'
+  }
+
+  if (props.method === 0) {
+    method = 'Cryptocurrency'
+  }
+
+  return (
+    <>
+      {open && (
+        <Modalx
+          fromName={props.clientName}
+          fromEmail={props.emailAddress}
+          fromInvoice={props.invoiceID}
+          fromDate={props.date}
+          fromMethod={method}
+          closeModal={open_modal}
+        />
+      )}
+      <tr onClick={open_modal}>
+        <td id="fromName">{props.clientName}</td>
+        <td id="fromEmailAddress">{props.emailAddress}</td>
+        <td id="invoiceID">{props.invoiceID}</td>
+        <td>₦{(props.amount * 1).toLocaleString('en-US')}</td>
+        <td id="issueDate">{props.date}</td>
+        <td>{status}</td>
+      </tr>
+    </>
+  )
+}
+
+export default DashboardPaymentHistory
